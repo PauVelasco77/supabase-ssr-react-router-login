31fabf5ff21a36e69b15a1b6398c1717e1821a6e
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-27 08:54:49 +0200

    refactor: remove Supabase loader and associated environment variable handling from root component

--- app/root.tsx
+++ app/root.tsx
@@ -5,12 +5,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
 } from 'react-router';
 
 import type { Route } from './+types/root';
 import './app.css';
-import { createClient } from './lib/supabase/server';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -24,30 +22,6 @@ export const links: Route.LinksFunction = () => [
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
 ];
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const ENV_VARS = {
-    SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
-    SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
-  };
-
-  const hasEnvVars = !!(ENV_VARS.SUPABASE_URL && ENV_VARS.SUPABASE_ANON_KEY);
-
-  let user = undefined;
-  if (hasEnvVars) {
-    const { supabase } = await createClient(request);
-    const {
-      data: { user: supabaseUser },
-    } = await supabase.auth.getUser();
-    user = supabaseUser;
-  }
-
-  return {
-    user,
-    hasEnvVars,
-    ENV_VARS,
-  };
-}
 
 export function meta() {
   return [
@@ -64,12 +38,6 @@ export function meta() {
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const data = useLoaderData() as {
-    ENV_VARS: {
-      SUPABASE_URL: string;
-      SUPABASE_ANON_KEY: string;
-    };
-  };
   return (
     <html lang="en">
       <head>
@@ -82,13 +50,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
-
-        {/* Inject ENV vars after Scripts */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(data.ENV_VARS)}`,
-          }}
-        />
       </body>
     </html>
   );
